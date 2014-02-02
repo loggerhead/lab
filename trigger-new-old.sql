67be956f1a861bb004f5d8b013af339ec8c6f05b
Fangzheng Long <lloggerhead@gmail.com>
2014-02-02 11:28:03 +0800

    Update trigger-new-old.sql

--- trigger-new-old.sql
+++ trigger-new-old.sql
@@ -1,4 +1,6 @@
 -- http://www.postgresql.org/docs/9.2/static/plpgsql-trigger.html
+set client_min_messages = error;
+
 DROP TABLE IF EXISTS foo CASCADE;
 CREATE TABLE foo (
     tid serial PRIMARY KEY
@@ -9,10 +11,13 @@ CREATE OR REPLACE FUNCTION foo_t()
 AS $$
 BEGIN
     IF (TG_OP = 'DELETE') THEN
+        RAISE NOTICE 'delete op';
         RETURN OLD; -- NEW is NULL
     ELSIF (TG_OP = 'UPDATE') THEN
+        RAISE NOTICE 'update op';
         RETURN OLD; -- NEW;
     ELSIF (TG_OP = 'INSERT') THEN
+        RAISE NOTICE 'insert op';
         RETURN NEW; -- OLD is NULL
     END IF;
     RETURN NULL;
@@ -22,3 +27,8 @@ $$ LANGUAGE plpgsql;
 DROP TRIGGER IF EXISTS foo_t ON foo;
 CREATE TRIGGER foo_t BEFORE DELETE OR UPDATE OR INSERT ON foo
    FOR EACH ROW EXECUTE PROCEDURE foo_t(); 
+
+insert into foo values (1);
+insert into foo values (2);
+update foo set tid = tid + 1;
+delete from foo;
