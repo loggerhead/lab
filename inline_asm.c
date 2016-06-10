3e4353cac85bad8d764ed004783d63cbdc16365c
loggerhead <lloggerhead@gmail.com>
2016-06-10 10:19:11 +0800

    add comments about reference

--- inline_asm.c
+++ inline_asm.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+// GCC Assembler Syntax: https://www.ibiblio.org/gferg/ldp/GCC-Inline-Assembly-HOWTO.html#s3
 int add(int x, int y)
 {
     int res;
+    /*
+     * +---+--------------------+
+     * | r |    Register(s)     |
+     * +---+--------------------+
+     * | a |   %eax, %ax, %al   |
+     * | b |   %ebx, %bx, %bl   |
+     * | c |   %ecx, %cx, %cl   |
+     * | d |   %edx, %dx, %dl   |
+     * | S |   %esi, %si        |
+     * | D |   %edi, %di        |
+     * +---+--------------------+
+     */
     asm("mov %1, %%eax\n"    // %eax = x
         "mov %2, %%ecx\n"    // %ecx = y
         "add %%ecx, %%eax\n" // %eax += %ecx
